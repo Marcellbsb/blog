7db3674c0c7ed3ec7209be2e50c5a5f11608b115
Marcelo Ramos <marcelobsb83@gmail.com>
2025-07-28 12:46:16 -0300

    inserindo imgs e iniciando estilizacao

--- Assets/assets.js
+++ Assets/assets.js
@@ -1,5 +1,7 @@
 import logo from '../Assets/logo.png';
 import arrow from '../Assets/arrow.png'
+import blog_pic_01 from '../Assets/blog_pic_01.png'
+import profile_icon from '../Assets/profile_icon.png'
 
 
 
@@ -8,5 +10,37 @@ import arrow from '../Assets/arrow.png'
 
 export const assets ={
     logo,
-    arrow
+    arrow,
+    profile_icon,
+    blog_pic_01,
+    
 }
+
+
+export const blog_data = [ 
+{
+    id:1,
+    title: 'A detailed step by step guide to manage your lifestyle',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore nemo, animi illo blanditiis vel dicta minima dignissimos doloribus atque accusamus sint distinctio facilis quas ducimus quam fugit, natus quidem. Deleniti?',
+    image: blog_pic_01,
+    date: Date.now(),
+    category: 'Lifestyle',
+    author: 'Kerollayne Ramos',
+    author_img: profile_icon
+
+},
+
+
+{
+    id:2,
+    title: 'A detailed step by step guide to manage your lifestyle',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore nemo, animi illo blanditiis vel dicta minima dignissimos doloribus atque accusamus sint distinctio facilis quas ducimus quam fugit, natus quidem. Deleniti?',
+    image: 'blog_pic_02',
+    date: Date.now(),
+    category: 'Lifestyle',
+    author: 'Kerollayne Ramos',
+    author_img: profile_icon
+
+}
+
+]
--- Components/Header.jsx
+++ Components/Header.jsx
@@ -8,8 +8,16 @@ const Header = () => {
         <div className='py-0 top-0 px-5 md:px-12 lg:px-28 '>
             <div className='flex justify-between items-center  '>
                 <Image src={assets.logo} width={160} alt="" className="w-[130px] sm:w-auto"/>
-                <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black'>Get Started <Image src={assets.arrow} alt=""/></button>
+                <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border  text-blue-500 border-solid border-black shadow-[-7px_7px_0px_#000000]'>Get Started <Image src={assets.arrow} alt=""/></button>
 
+            </div>
+            <div className='text-center my-8'>
+                <h1 className='text-3xl sm:text-5xl font-medium text-blue-500'>Latest Blogs</h1>
+                <p className='mt-10 max-w-[740px] m-auto text-xs sm:text-base'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore nemo, animi illo blanditiis vel dicta minima dignissimos doloribus atque accusamus sint distinctio facilis quas ducimus quam fugit, natus quidem. Deleniti?</p>
+                <form className='flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]' action=''>
+                    <input type='email' placeholder='Enter your email' className='pl-4 outline-none'/>
+                    <button type='submit' className='border-1  text-blue-500 border-black py-4 px-4 sm:px-8 active:bg-gray-700 active:text-white'>Subscribe</button>
+                </form>
             </div>
 
         </div>
