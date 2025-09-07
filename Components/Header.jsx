9482cee733f5bce9a92542acadcd02f4ff6c4301
Marcelo Ramos <marcelobsb83@gmail.com>
2025-09-07 09:40:26 -0300

    insercao de novos blogs

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -5,43 +5,66 @@ import React, { useState } from "react";
 import { toast } from "react-toastify";
 
 const Header = () => {
+  const [email, setEmail] = useState("");
 
-    const [email,setEmail] = useState("");
-
-    const onSubmitHandler = async(e) =>{
-        e.preventDefault();
-        const formData = new FormData();
-        formData.append("email",email);
-        const response = await axios.post('/api/email',formData);
-        if (response.data.sucess) {
-            toast.success (response.data.msg);
-            setEmail ("");
-        }
-        else {
-            toast.error("Error");
-        }
-
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("email", email);
+    const response = await axios.post("/api/email", formData);
+    if (response.data.sucess) {
+      toast.success(response.data.msg);
+      setEmail("");
+    } else {
+      toast.error("Error");
     }
+  };
 
-    return (
-        <div className='top-0 pt-8 px-5 md:px-12 lg:px-28 '>
-            <div className='flex justify-between items-center  '>
-                <Image src={assets.logo} width={140} alt="" className="w-[130px] sm:w-auto"/>
-                <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border  text-blue-500 border-solid border-black shadow-[-7px_7px_0px_#000000]'>Get Started <Image src={assets.arrow} alt=""/></button>
+  return (
+    <div className="top-0 pt-8 px-5 md:px-12 lg:px-28 ">
+      <div className="flex justify-between items-center  ">
+        <Image
+          src={assets.logo}
+          width={140}
+          alt=""
+          className="w-[130px] sm:w-auto"
+        />
+        <button className="flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border  text-blue-500 border-solid border-black shadow-[-7px_7px_0px_#000000]">
+          Get Started <Image src={assets.arrow} alt="" />
+        </button>
+      </div>
+      <div className="text-center my-8">
+        <h1 className="text-3xl sm:text-5xl font-medium text-blue-500">
+          Latest Blogs
+        </h1>
+        <p className="mt-10 max-w-[740px] m-auto text-xs sm:text-base">
+          Acompanhe nossos artigos mais recentes sobre o que está em alta no
+          mundo da tecnologia, startups e negócios digitais. Conteúdo atualizado
+          e direto ao ponto.
+        </p>
 
-            </div>
-            <div className='text-center my-8'>
-                <h1 className='text-3xl sm:text-5xl font-medium text-blue-500'>Latest Blogs</h1>
-                <p className='mt-10 max-w-[740px] m-auto text-xs sm:text-base'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore nemo, animi illo blanditiis vel dicta minima dignissimos doloribus atque accusamus sint distinctio facilis quas ducimus quam fugit, natus quidem. Deleniti?</p>
-                <form onSubmit={onSubmitHandler} className='flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]' action=''>
-                    <input onChange={(e)=>setEmail(e.target.value)} value={email} type='email' placeholder='Enter your email' className='pl-4 outline-none'/>
-                    <button type='submit' className='border-1  text-blue-500 border-black py-4 px-4 sm:px-8 active:bg-gray-700 active:text-white'>Subscribe</button>
-                </form>
-            </div>
-
-        </div>
-    );
-
-}
+        <form
+          onSubmit={onSubmitHandler}
+          className="flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]"
+          action=""
+        >
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            type="email"
+            placeholder="Enter your email"
+            className="pl-4 outline-none"
+          />
+          <button
+            type="submit"
+            className="border-1  text-blue-500 border-black py-4 px-4 sm:px-8 active:bg-gray-700 active:text-white"
+          >
+            Subscribe
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
